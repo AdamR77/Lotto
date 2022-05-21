5f635d531ea3da50bce60f6e41d66978828bde7c
Adam <adambox77@gmail.com>
2022-05-21 16:13:53 +0200

    po mod. zepsute

--- totolotek_02.py
+++ totolotek_02.py
@@ -1,60 +1,74 @@
-import random
+from random import sample
 
-tablica = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,
-34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49]
-
-podane = []
-i =0
-
+randomized = []
+table = []
+for nr in range(1,50):
+    table.append(nr)
+   
 print("* * * * * * * * * * * *")
 print("* * *  L O T T O  * * *")
 print("* * * * * * * * * * * * \n")
 
-for x in range(6):
-    i+=1    
+your_nrs = []
+i =0
+draw = 1 #liczba kolejna zakładów
+hit = 0 
+price = 3 #cena zakładu
+while True:
+    print(f"losowanie nr: {draw}")
+    for x in range(6):
+        i+=1    
     
-    while True:
-        print("Liczba ", i, " :", end =" ")
         while True:
-            try:
-                liczba = int(input("podaj liczbe z przedziału 1-49: " ))
-            except:
-                print("nie podałeś liczby")
-            else:
-                break
+            print(f"Liczba {i}: ", end =" ")
+            while True:
+                try:
+                    nr = int(input("podaj liczbe całkowitą z przedziału 1-49: " ))
+                except:
+                    print("nie podałeś liczby ale litery albo inne znaki")
+                else:
+                    break
 
-        if liczba < 1 or liczba > 49:
-            print("Liczba poza przedziałem 1-49")  
-        elif liczba in podane:
-            print("Liczby nie mogą się powtarzać")
-        else:
-            podane.append(liczba)
-            break
+            if nr < 1 or nr > 49:
+                print("Liczba poza przedziałem 1-49")  
+            elif nr in your_nrs:
+                print(f"Już podałeś {nr}, liczby nie mogą się powtarzać")
+            else:
+                your_nrs.append(nr)
+                break
             
 
-print(" Twoje liczby to: ", podane, "\n" )  
-wylosowane = random.sample(tablica, k = 6)
-print(" wylosowane liczby to: ", wylosowane, "\n")
+    print(f" Twoje liczby to: {your_nrs} \n" )  
+    randomized = sample(table, k = 6)
+    print(f" wylosowane liczby to: {randomized} \n")
 
-trafione = 0
-for x in range(6):
-    if podane[x] in wylosowane:
-        trafione += 1
-        print(" trafiłeś: ", podane[x]  )
+    hit = 0 #liczba trafień w losowaniu
+    for x in range(5):
+        if your_nrs[x] in randomized:
+            hit += 1
+            print(" trafiłeś: ", {your_nrs[x]}  )
 
-if trafione == 3:
-    print("3 trafione")
-if trafione == 4:
-    print("4 trafione")
-if trafione == 5:
-    print("5 trafionych")
-if trafione == 6:
-    print("6 trafionych. GRATULACJE !!! stałeś sie bogaty :) ")
-else:
-    print("Nie wygrałeś żadnej nagrody (trafienia 3,4,5 lub 6 liczb)")
+        if hit >= 3:
+            print("trafiłeś {hit} liczb(y)")
+        if hit == 6:
+            print("GRATULACJE !!! stałeś sie bardzo bogaty :) ")
+    
+        else:
+            print("Nie wygrałeś żadnej nagrody (trafienia 3,4,5 lub 6 liczb)")
+        quote = price * draw
+        print(f"wydałeś {quote} złotych")    
+    while True:
+        decision = input("chcesz grać dalej? [t/n]")
+        if decision == "t" or "n":
+            break
+        if decision == "n":
+            print(" Dzięki. Kończymy")
+            break
+        draw += 1
+        break
+        
 
-a = input("press any key")
+
 
 
 
